f3640b27dc99dc0dff50f242c750b3f05d85854a
codomposer <akileus0902@gmail.com>
2024-07-30 23:41:19 -0400

    fix(babelrc, eslint): update the babel & eslint configuration

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,22 @@
 function override(config) {
   // Ensure config.resolve is defined
-  config.resolve = config.resolve || {}
+  config.resolve = config.resolve || {};
 
   // Ensure config.resolve.fallback is defined
-  config.resolve.fallback = config.resolve.fallback || {}
+  config.resolve.fallback = config.resolve.fallback || {};
 
   // Add polyfills for Node.js core modules
   config.resolve.fallback = {
     ...config.resolve?.fallback,
-    "http": require.resolve("stream-http"),
-    "https": require.resolve('https-browserify'),
-    "url": require.resolve('url/'),
-    "buffer": require.resolve('buffer/'),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    url: require.resolve("url/"),
+    buffer: require.resolve("buffer/")
   };
 
   return config;
 }
 
 module.exports = {
-  override,
+  override
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,38 +6,36 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 export default [
-    {
-        ignores: ["**/coverage/", "**/dist/"],
-    },
-    ...compat.extends(
-        'eslint:recommended',
-        'plugin:react/recommended',
-    ),
-    {
-        rules: {
-            'no-console': 'warn',
-            "react/prop-types": "off",
-        },
-    },
-    {
-        files: ["**/*.js", "**/*.mjs"],
-        languageOptions: {
-          globals: {
-            require: "readonly",
-            module: "readonly",
-            describe: "readonly",
-            test: "readonly",
-            process: "readonly",
-            document: "readonly",
-            expect: "readonly",
-            __dirname: "readonly",
-          },
-        },
-      },
+  {
+    ignores: ["**/coverage/", "**/dist/"]
+  },
+  ...compat.extends("eslint:recommended", "plugin:react/recommended"),
+  {
+    rules: {
+      "no-console": "warn",
+      "react/prop-types": "off"
+    }
+  },
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        describe: "readonly",
+        test: "readonly",
+        process: "readonly",
+        document: "readonly",
+        expect: "readonly",
+        __dirname: "readonly",
+        console: "readonly"
+      }
+    }
+  }
 ];
